70fef50efcbbf827a5285d11c1dd754e1f333482
NurulIslam17 <nurulcse09@gmail.com>
2026-01-09 21:54:16 +0600

    Dashboard count shown

--- src/main/java/com/nurul/taskap/controller/DashboardController.java
+++ src/main/java/com/nurul/taskap/controller/DashboardController.java
@@ -1,15 +1,25 @@
 package com.nurul.taskap.controller;
 
+import com.nurul.taskap.repository.DashboardCountProjection;
+import com.nurul.taskap.service.DashboardService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DashboardController {
 
+    private final DashboardService dashboardService;
+
+    public DashboardController(DashboardService dashboardService) {
+        this.dashboardService = dashboardService;
+    }
 
     @GetMapping("/dashboard")
-    public String index()
+    public String index(Model model)
     {
+        DashboardCountProjection dashboardCountProjection = dashboardService.getDashboardCounts();
+        model.addAttribute("dashboard",dashboardCountProjection);
         return "dashboard/index";
     }
 }
---
+++ src/main/java/com/nurul/taskap/entity/AppUser.java
@@ -0,0 +1,67 @@
+package com.nurul.taskap.entity;
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "app_users")
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String userName;
+    private String password;
+    private String description;
+
+
+    public AppUser() {}
+
+    public AppUser(Long id, String name, String userName, String password, String description) {
+        this.id = id;
+        this.name = name;
+        this.userName = userName;
+        this.password = password;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
