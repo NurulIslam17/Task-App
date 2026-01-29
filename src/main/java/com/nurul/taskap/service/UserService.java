4fd1fe7d92eede0e5f584c2ac3d6e31c5ec04083
NurulIslam17 <nurulcse09@gmail.com>
2026-01-29 23:39:13 +0600

    Role Save

--- src/main/java/com/nurul/taskap/service/UserService.java
+++ src/main/java/com/nurul/taskap/service/UserService.java
@@ -3,21 +3,29 @@ package com.nurul.taskap.service;
 import com.nurul.taskap.dto.user.UserDto;
 import com.nurul.taskap.dto.user.UserRequestDto;
 import com.nurul.taskap.entity.AppUser;
+import com.nurul.taskap.entity.Role;
+import com.nurul.taskap.repository.RoleRepository;
 import com.nurul.taskap.repository.UserRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
     private final ModelMapper modelMapper;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository, ModelMapper modelMapper) {
+    public UserService(UserRepository userRepository, RoleRepository roleRepository, ModelMapper modelMapper, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
         this.modelMapper = modelMapper;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public List<UserDto> getUserList() {
@@ -58,7 +66,12 @@ public class UserService {
 
 
     public void updateUser(UserRequestDto userRequestDto, Long id) {
+
+        System.out.println(userRequestDto);
         AppUser  userData = modelMapper.map(userRequestDto, AppUser.class);
+        Role roles = roleRepository.findByName("ROLE_TL");
+        userData.setPassword(passwordEncoder.encode("12345"));
+        userData.setRoles(Arrays.asList(roles));
         userData.setId(id);
         userRepository.save(userData);
     }
