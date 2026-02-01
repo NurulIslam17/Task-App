38f48b9ecdcd3b21d1696ee2ccb5b1b2dc38c68a
NurulIslam17 <nurulcse09@gmail.com>
2026-02-01 21:24:49 +0600

    Assigned task

--- src/main/java/com/nurul/taskap/service/TaskAssignService.java
+++ src/main/java/com/nurul/taskap/service/TaskAssignService.java
@@ -1,5 +1,7 @@
 package com.nurul.taskap.service;
 
+import com.nurul.taskap.dto.taskAssign.TaskAssignRequestDto;
+import com.nurul.taskap.entity.TaskAssign;
 import com.nurul.taskap.repository.TaskAssignRepository;
 import org.springframework.stereotype.Service;
 
@@ -10,5 +12,15 @@ public class TaskAssignService {
 
     public TaskAssignService(TaskAssignRepository taskAssignRepository) {
         this.taskAssignRepository = taskAssignRepository;
+    }
+
+    public void assignTask(TaskAssignRequestDto taskAssignRequestDto) {
+
+        TaskAssign taskAssign = new TaskAssign();
+        taskAssign.setTask_id(taskAssignRequestDto.getTask_id());
+        taskAssign.setUser_id(taskAssignRequestDto.getUser_id());
+        taskAssign.setTl_id(taskAssignRequestDto.getTl_id());
+        taskAssign.setDescription(taskAssignRequestDto.getDescription());
+        taskAssignRepository.save(taskAssign);
     }
 }
