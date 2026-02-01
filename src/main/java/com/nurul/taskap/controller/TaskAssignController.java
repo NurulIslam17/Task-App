c10cf0bc2b7948f58e6b177e75abd94490ec2d28
NurulIslam17 <nurulcse09@gmail.com>
2026-02-01 21:13:29 +0600

    Assign form submit

--- src/main/java/com/nurul/taskap/controller/TaskAssignController.java
+++ src/main/java/com/nurul/taskap/controller/TaskAssignController.java
@@ -2,11 +2,15 @@ package com.nurul.taskap.controller;
 
 import com.nurul.taskap.dto.task.TaskDto;
 import com.nurul.taskap.dto.taskAssign.TaskAssignRequestDto;
+import com.nurul.taskap.dto.user.UserDto;
 import com.nurul.taskap.service.TaskAssignService;
 import com.nurul.taskap.service.TaskService;
+import com.nurul.taskap.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -16,19 +20,33 @@ public class TaskAssignController {
 
     private final TaskAssignService taskAssignService;
     private final TaskService taskService;
+    private final UserService userService;
 
-    public TaskAssignController(TaskAssignService taskAssignService, TaskService taskService) {
+    public TaskAssignController(TaskAssignService taskAssignService, TaskService taskService, UserService userService) {
         this.taskAssignService = taskAssignService;
         this.taskService = taskService;
+        this.userService = userService;
     }
     @GetMapping("/task/assign")
     public String assignTaskToUser(@RequestParam("id") Long id, Model model)
     {
         TaskDto taskDto = taskService.findById(id);
         TaskAssignRequestDto taskAssignRequestDto = new TaskAssignRequestDto();
+        taskAssignRequestDto.setTask_id(id);
+        List<UserDto> users = userService.getUserList("ROLE_USER");
+        List<UserDto> teamLeads = userService.getUserList("ROLE_TL");
 
         model.addAttribute("taskAssignData", taskAssignRequestDto);
         model.addAttribute("taskDto", taskDto);
+        model.addAttribute("users",users);
+        model.addAttribute("tls",teamLeads);
         return "taskAssign/add";
+    }
+
+    @PostMapping("/task/assign")
+    public String assignTask(@ModelAttribute("taskAssignData") TaskAssignRequestDto taskAssignRequestDto)
+    {
+        System.out.println(taskAssignRequestDto);
+        return "redirect:/task/list";
     }
 }
