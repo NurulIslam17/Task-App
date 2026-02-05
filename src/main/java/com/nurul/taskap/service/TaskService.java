3f952bb94b4a48a75eb778334082a158193374f0
NurulIslam17 <nurulcse09@gmail.com>
2026-02-05 23:18:04 +0600

    User dashboard pending

--- src/main/java/com/nurul/taskap/service/TaskService.java
+++ src/main/java/com/nurul/taskap/service/TaskService.java
@@ -2,9 +2,11 @@ package com.nurul.taskap.service;
 
 import com.nurul.taskap.dto.task.TaskDto;
 import com.nurul.taskap.dto.task.TaskRequestDto;
+import com.nurul.taskap.entity.AppUser;
 import com.nurul.taskap.entity.Task;
 import com.nurul.taskap.enumType.TaskStatus;
 import com.nurul.taskap.repository.TaskRepository;
+import com.nurul.taskap.utils.SecurityUtil;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
@@ -16,11 +18,15 @@ public class TaskService {
 
     private final TaskRepository taskRepository;
     private final ModelMapper modelMapper;
+    private final UserService userService;
+    private final SecurityUtil securityUtil;
 
 
-    public TaskService(TaskRepository taskRepository, ModelMapper modelMapper) {
+    public TaskService(TaskRepository taskRepository, ModelMapper modelMapper, UserService userService, SecurityUtil securityUtil) {
         this.taskRepository = taskRepository;
         this.modelMapper = modelMapper;
+        this.userService = userService;
+        this.securityUtil = securityUtil;
     }
 
     public List<TaskDto> getTaskList() {
@@ -63,6 +69,17 @@ public class TaskService {
         return taskList
                 .stream()
                 .map(task -> modelMapper.map(task, TaskDto.class))
+                .toList();
+    }
+
+    public List<TaskDto> taskByAuthUserName() {
+
+        String name = securityUtil.getCurrentUsername();
+        AppUser appUser = userService.findByName(name);
+        List<Task> taskList = taskRepository.findTasksByUserId(appUser.getId());
+        return taskList
+                .stream()
+                .map(task -> modelMapper.map(task,TaskDto.class))
                 .toList();
     }
 }
